b1aff31dc9cfd4d31d3ecc7885fe0ef654c7a39c
klentoy-lab <klentdagsa21@gmail.com>
2026-05-21 22:45:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 NTFY_TOPIC = "Seraphim_Protocol_Gold_99283"
 TARGET_EMAIL = "klentdagsa21@gmail.com"
 VOICE_CODE = "en-CA-LiamNeural"
-BGM_FILE = "NIKI - Paths (Instrumental).mp3"
+BGM_FILE = "how to never stop being sad (instrumental) - dandelion hands [slowed] [reverb].mp3"
 
 # ============================================================================
 # 1.5 CREATOR BACKDOOR & SECURITY CHECK
